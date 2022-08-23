31c72dfe0bb48b43e04db442a29bd85501cf387e
Viktoriia Lomtieva <vikki.sora@gmail.com>
2022-08-23 13:42:15 +0300

    [header] template created

---
+++ src/components/partials/header/header.jsx
@@ -0,0 +1,44 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+import "./header.css"
+import UserProfilePreview from "../userProfilePreview/userProfilePreview"
+import avatar from "../../../assets/avatars/anna-may-avatar.jpg"
+import logo from "../../../assets/logo/logo-blue.png"
+import Image from "../../primitives/image/image"
+import Button from "../../primitives/button/button"
+import PropTypes from "prop-types"
+
+function Header({isAuthorized}) {
+
+  return (
+    <div className="header">
+      <div className="header_logo">
+        <Image src={logo} fit="contain"/>
+      </div>
+      {/*<div className="header_nav">*/}
+      {/*  header:*/}
+      {/*  <NavLink to="/">home</NavLink>*/}
+      {/*  <NavLink to="/user/1">1</NavLink>*/}
+      {/*  <NavLink to="/user/2">2</NavLink>*/}
+      {/*  <NavLink to="/user/name">name</NavLink>*/}
+      {/*  <NavLink to="/qwert">qwert</NavLink>*/}
+      {/*</div>*/}
+      <div className="header_authorize">
+        { isAuthorized ?
+          <UserProfilePreview image={avatar} name="Anna May" small={true}/> :
+          <Button variant="transparent" label="Sign Up" onClick="/" />
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Header
+
+Header.propTypes = {
+  isAuthorized: PropTypes.bool,
+}
+
+Header.defaultProps = {
+  isAuthorized: false
+}
---
+++ src/components/partials/header/header.stories.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import Header from "./header"
+
+export default {
+  title: "partials/header",
+  component: Header,
+  argTypes: {
+    isAuthorized: { boolean: { action: false } },
+  }
+}
+
+export function UnauthorizedHeader(args) {
+  return <Header {...args}/>
+}
+
+UnauthorizedHeader.args = {
+  isAuthorized: false,
+}
+
+export function AuthorizedHeader(args) {
+  return <Header {...args}/>
+}
+
+AuthorizedHeader.args = {
+  isAuthorized: true,
+}
