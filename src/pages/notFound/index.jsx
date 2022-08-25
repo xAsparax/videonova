060d5a0c2dcc2811c759fa08f1a35472679982d4
Viktoriia Lomtieva <vikki.sora@gmail.com>
2022-08-25 11:13:20 +0300

    [notFoundPage] updated

--- src/pages/notFound/index.jsx
+++ src/pages/notFound/index.jsx
@@ -1,8 +1,26 @@
 import React from "react"
 import Layout from "../../layout"
+import Heading from "../../components/primitives/heading/heading"
+import "./notFound.css"
+import Button from "../../components/primitives/button/button"
+import {NavLink} from "react-router-dom"
 
 function NotFoundPage() {
-  return <Layout>404 - not found!</Layout>
+  return <Layout>
+    <div className="notFound">
+      <div className="notFound_message">
+        <Heading weight="1">
+          404 - not found!
+        </Heading>
+      </div>
+      <div className="notFound_button">
+        <span> Go to </span>
+        <NavLink to="/">
+          <Button onClick="/" variant="prime">Homepage</Button>
+        </NavLink>
+      </div>
+    </div>
+  </Layout>
 }
 
 export default NotFoundPage
