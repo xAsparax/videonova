927ba72c79e702210f02c479d1c91a02003e7237
Viktoriia Lomtieva <vikki.sora@gmail.com>
2022-08-26 22:29:35 +0300

    [modal] storybook created

---
+++ src/components/partials/modal/modal.stories.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Modal from "./modal"
+
+export default {
+  title: "partials/modal",
+  component: Modal,
+  argTypes: {
+    show: {control: "boolean"},
+  }
+}
+
+export function Primary(args) {
+  return (
+    <Modal {...args}>
+      <div style={{ width: "644px", height: "861px", backgroundColor: "#FFFFFF" }}></div>
+    </Modal>
+  )
+}
+
+Primary.args = {
+  show: true,
+}
