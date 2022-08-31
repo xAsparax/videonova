2b17dd5585ed754fef6250dbde4f982832572b24
Viktoriia Lomtieva <vikki.sora@gmail.com>
2022-08-31 18:08:07 +0300

    layout updated with authorized selector

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,18 +1,23 @@
-import React from "react"
+import React, {useState} from "react"
+import { useSelector } from "react-redux"
 import PropTypes from "prop-types"
 import Header from "../components/partials/header/header"
 import Footer from "../components/partials/footer/footer"
 import "./style.css"
+import { selectAuthorized, selectUserId, selectUserName, selectUserImage } from "../store/modules/user"
 
 function Layout({ children }) {
+
+  const isAuthorized = useSelector(selectAuthorized)
+
   return (
     <div className="layout">
       <div className="layout__header">
-        <Header />
+        <Header isAuthorized={isAuthorized} />
       </div>
       <div className="layout__content">{children}</div>
       <div className="siteContent__footer">
-        <Footer text="All Rights Reserved 2022"/>
+        <Footer />
       </div>
     </div>
   )
